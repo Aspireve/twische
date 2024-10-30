1ec77fb57e666331605cf32b2db96484aa7e381e
Steve Fernandes <steve1818fernandes@gmail.com>
2024-10-30 15:57:00 +0530

    [CHORE] Fixed the API and Migrated it to V2

--- pages/api/addTweet/index.ts
+++ pages/api/addTweet/index.ts
@@ -4,13 +4,22 @@ import { PrismaClient } from "@prisma/client";
 // Initialize Prisma Client
 const prisma = new PrismaClient();
 
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     // Create a new user
+    const { tweet, postedTime } = req.body;
+    try {
+      const newtweet = await prisma.tweets.create({
+        data: { tweet, postedTime },
+      });
+      res.status(201).json(newtweet);
+    } catch (error: unknown) {
+      res.status(500).json({ message: "Error creating user", error });
+    }
+  } else if (req.method === "GET") {
     const { tweet, postedTime } = req.body;
     try {
       const newtweet = await prisma.tweets.create({
